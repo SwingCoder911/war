9561a068160e42ec3e16706d2a8a22011c7a82f0
unknown <kaleb2azriel@gmail.com>
2017-08-25 00:06:33 -0700

    Adding Mocha

---
+++ test/TestGame.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Game', function() {
+  describe('#Test()', function() {
+    it('should assert true', function() {
+      assert(true);
+    });
+  });  
+});
